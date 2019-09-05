fcc6339ee75555b3094a0a5d1bc57ee578b614d3
陈隆霄 <chenlongxiao@inspur.com>
2019-09-05 21:16:19 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+import ThemeBlue from './lib/theme-blue'
+import ThemeWeChat from './lib/theme-wechat'
+import ThemeAlipay from './lib/theme-alipay'
+import ThemeFacebook from './lib/theme-facebook'
+import ThemeQQ from './lib/theme-qq'
+import ThemeAuto from './lib/theme-auto'
+
+import Button from './lib/button'
+import Dialog from './lib/dialog'
+import Alert from './lib/alert'
+import Gallery from './lib/gallery'
+import Navigation from './lib/navigation'
+import Tab from './lib/tab'
+import Search from './lib/search'
+import ListView from './lib/listview'
+import Loading from './lib/loading'
+
+import FormLabelField from './lib/form-label-field'
+import FormLabelInput from './lib/form-label-input'
+import FormLabelPassword from './lib/form-label-password'
+import FormLabelSwitch from './lib/form-label-switch'
+
+import PickerCamera from './lib/picker-camera'
+import PickerAddress from './lib/picker-address'
+import PickerCategoryA from './lib/picker-category-a'
+import PickerCategoryB from './lib/picker-category-b'
+import PickerMapnav from './lib/picker-mapnav'
+import PickerDate from './lib/picker-date'
+
+import TipsA from './lib/tips-a'
+import TipsB from './lib/tips-b'
+import TipsC from './lib/tips-c'
+
+import TitleBarA from './lib/titlebar-a'
+import TitleBarB from './lib/titlebar-b'
+import TitleBarC from './lib/titlebar-c'
+
+import SkeletonAggregation from './lib/skeleton-aggregation'
+import SkeletonDetail from './lib/skeleton-detail'
+import SkeletonListView from './lib/skeleton-listview'
+import SkeletonNews from './lib/skeleton-news'
+
+import AuthLogin from './lib/auth-login'
+import AuthReal from './lib/auth-real'
+
+import Card from './lib/card'
+import Warning from './lib/warning'
+
+import FormGroup from './lib/form-group'
+
+import ToastPlugin from './lib/toast-plugin'
+
+export {
+    ThemeBlue,
+    ThemeWeChat,
+    ThemeAlipay,
+    ThemeFacebook,
+    ThemeQQ,
+    ThemeAuto,
+    Button, 
+    FormGroup, 
+    FormLabelField, 
+    FormLabelInput, 
+    FormLabelPassword,
+    FormLabelSwitch,
+    Dialog,
+    Alert, 
+    Gallery,
+    PickerCamera,
+    PickerAddress,
+    PickerCategoryA,
+    PickerCategoryB,
+    PickerMapnav,
+    PickerDate,
+    Navigation,
+    Tab,
+    Search,
+    ListView,
+    Loading,
+    TipsA,
+    TipsB,
+    TipsC,
+    TitleBarA,
+    TitleBarB,
+    TitleBarC,
+    SkeletonListView,
+    SkeletonAggregation,
+    SkeletonDetail,
+    SkeletonNews,
+    AuthLogin,
+    AuthReal,
+    Card,
+    Warning,
+    ToastPlugin
+}
